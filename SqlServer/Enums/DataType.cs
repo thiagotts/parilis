23dbd965d966fee7a4a47951e6c096e6c2689ef2
Thiago Sá <thiagottsa@gmail.com>
2015-02-07 19:47:01 -0300

    Added DataType enum.

---
+++ SqlServer/Enums/DataType.cs
@@ -0,0 +1,110 @@
+﻿using System.ComponentModel;
+
+namespace SqlServer.Enums {
+    internal enum DataType {
+        [DefaultValue("bigint")]
+        BigInt,
+
+        [DefaultValue("numeric")]
+        Numeric,
+
+        [DefaultValue("bit")]
+        Bit,
+
+        [DefaultValue("smallint")]
+        Smallint,
+
+        [DefaultValue("decimal")]
+        Decimal,
+
+        [DefaultValue("smallmoney")]
+        Smallmoney,
+
+        [DefaultValue("int")]
+        Int,
+
+        [DefaultValue("tinyint")]
+        Tinyint,
+
+        [DefaultValue("money")]
+        Money,
+
+        [DefaultValue("float")]
+        Float,
+
+        [DefaultValue("real")]
+        Real,
+
+        [DefaultValue("date")]
+        Date,
+
+        [DefaultValue("datetimeoffset")]
+        Datetimeoffset,
+
+        [DefaultValue("datetime2")]
+        Datetime2,
+
+        [DefaultValue("smalldatetime")]
+        Smalldatetime,
+
+        [DefaultValue("datetime")]
+        Datetime,
+
+        [DefaultValue("time")]
+        Time,
+
+        [DefaultValue("char")]
+        Char,
+
+        [DefaultValue("varchar")]
+        Varchar,
+
+        [DefaultValue("text")]
+        Text,
+
+        [DefaultValue("nchar")]
+        Nchar,
+
+        [DefaultValue("nvarchar")]
+        Nvarchar,
+
+        [DefaultValue("ntext")]
+        Ntext,
+
+        [DefaultValue("binary")]
+        Binary,
+
+        [DefaultValue("varbinary")]
+        Varbinary,
+
+        [DefaultValue("image")]
+        Image,
+
+        [DefaultValue("cursor")]
+        Cursor,
+
+        [DefaultValue("timestamp")]
+        Timestamp,
+
+        [DefaultValue("hierarchyid")]
+        Hierarchyid,
+
+        [DefaultValue("uniqueidentifier")]
+        Uniqueidentifier,
+
+        [DefaultValue("sql_variant")]
+        SqlVariant,
+
+        [DefaultValue("xml")]
+        Xml,
+
+        [DefaultValue("table")]
+        Table,
+
+        [DefaultValue("geography")]
+        Geography,
+
+        [DefaultValue("geometry")]
+        Geometry 
+    }
+}
---
+++ SqlServer/Enums/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace SqlServer.Enums {
+    public static class Enums {
+        public static string GetDefaultValue(object value) {
+            if (value == null) return string.Empty;
+            var fi = value.GetType().GetField(value.ToString());
+            var attributes = (DefaultValueAttribute[]) fi.GetCustomAttributes(typeof (DefaultValueAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Value.ToString() : value.ToString();
+        }
+    }
+}
