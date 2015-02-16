e21da16becb008cc7880bd1bcf8b2bb646e1b100
Thiago Sá <thiagottsa@gmail.com>
2015-02-15 23:16:51 -0300

    Initial implementation of Create method on Tables class.

--- SqlServer/Tables.cs
+++ SqlServer/Tables.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Core.Descriptions;
+using Core.Exceptions;
 using Core.Interfaces;
+using Microsoft.SqlServer.Management.Smo;
 
 namespace SqlServer {
     public class Tables : ITable {
+        private Database database;
+        private SqlServerDatabase sqlServerDatabase;
+
+        public Tables(Database database) {
+            this.database = database;
+            sqlServerDatabase = new SqlServerDatabase(database);
+        }
+
         public void Create(TableDescription tableDescription) {
-            throw new NotImplementedException();
+            if(sqlServerDatabase.GetTable(tableDescription.Schema, tableDescription.Name) != null)
+                throw new InvalidTableNameException();
+
+            var columns = new List<string>();
+            foreach (var column in tableDescription.Columns) {
+                columns.Add(string.Format("[{0}] [{1}]{2} {3}", column.Name, column.Type,
+                    string.IsNullOrWhiteSpace(column.MaximumSize) ? string.Empty : string.Format("({0})", column.MaximumSize),
+                    column.AllowsNull ? "NULL" : "NOT NULL"));
+            }
+
+            database.ExecuteNonQuery(string.Format(@"CREATE TABLE [{0}].[{1}]({2})",
+                tableDescription.Schema, tableDescription.Name, string.Join(",", columns)));
         }
 
         public void Remove(string schema, string tableName) {
---
+++ Tests/Tests.SqlServer/TablesTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Descriptions;
+using Core.Exceptions;
+using NUnit.Framework;
+using SqlServer;
+using Tests.Core;
+
+namespace Tests.SqlServer {
+    [TestFixture]
+    public class TablesTests : DatabaseTest {
+        private Tables tables;
+        private SqlServerDatabase sqlServerDatabase;
+
+        [TestFixtureSetUp]
+        public override void InitializeClass() {
+            base.InitializeClass();
+            tables = new Tables(Database);
+            sqlServerDatabase = new SqlServerDatabase(Database);
+            Database.ExecuteNonQuery(@"CREATE SCHEMA testschema");
+        }
+
+        [TearDown]
+        public void FinishTest() {
+            var table = Database.Tables["TEST_TABLE_2"];
+            if (table != null) table.Drop();
+
+            table = Database.Tables["TEST_TABLE"];
+            if (table != null) table.Drop();
+        }
+
+        [Test]
+        public void IfThereIsNotAnotherTableWithTheSameNameInTheSameSchema_CreateMethodMustCreateTheTable() {
+            tables.Create(new TableDescription {
+                Schema = "dbo",
+                Name = "TEST_TABLE",
+                Columns = new List<ColumnDescription> {
+                    new ColumnDescription {
+                        Name = "id",
+                        Type = "varchar",
+                        MaximumSize = "150",
+                        AllowsNull = true
+                    }
+                }
+            });
+
+            TableDescription table = sqlServerDatabase.GetTable("dbo", "TEST_TABLE");
+
+            Assert.IsNotNull(table);
+            Assert.AreEqual("TEST_TABLE", table.Name);
+            Assert.AreEqual(1, table.Columns.Count);
+            Assert.AreEqual("id", table.Columns.Single().Name);
+            Assert.AreEqual("varchar", table.Columns.Single().Type);
+            Assert.AreEqual("150", table.Columns.Single().MaximumSize);
+            Assert.IsTrue(table.Columns.Single().AllowsNull);
+        }
+
+        [Test]
+        public void IfThereIsAnotherTableWithTheSameNameInAnotherSchema_CreateMethodMustCreateTheTable() {
+            Database.ExecuteNonQuery(@"CREATE TABLE [testschema].[TEST_TABLE](
+                [id] [bigint] NOT NULL,
+                [description] [nvarchar](max) NULL)");
+
+            tables.Create(new TableDescription {
+                Schema = "dbo",
+                Name = "TEST_TABLE",
+                Columns = new List<ColumnDescription> {
+                    new ColumnDescription {
+                        Name = "id",
+                        Type = "bigint"
+                    }
+                }
+            });
+
+            TableDescription table = sqlServerDatabase.GetTable("dbo", "TEST_TABLE");
+
+            Assert.IsNotNull(table);
+            Assert.AreEqual("TEST_TABLE", table.Name);
+            Assert.AreEqual(1, table.Columns.Count);
+            Assert.AreEqual("id", table.Columns.Single().Name);
+            Assert.AreEqual("bigint", table.Columns.Single().Type);
+            Assert.IsFalse(table.Columns.Single().AllowsNull);
+        }
+
+        [Test]
+        public void IfThereIsAnotherTableWithTheSameNameInTheSameSchema_CreateMethodMustThrowException() {
+            Database.ExecuteNonQuery(@"CREATE TABLE [dbo].[TEST_TABLE](
+                [id] [bigint] NOT NULL,
+                [description] [nvarchar](max) NULL)");
+
+            Assert.Throws<InvalidTableNameException>(() => tables.Create(new TableDescription {
+                Schema = "dbo",
+                Name = "TEST_TABLE",
+                Columns = new List<ColumnDescription> {
+                    new ColumnDescription {
+                        Name = "id",
+                        Type = "bigint"
+                    }
+                }
+            }));
+        }
+    }
+}
