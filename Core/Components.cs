1e29a2519407cf9e73fdcdfe043feece7423fafd
Thiago Sá <thiagottsa@gmail.com>
2015-02-17 05:43:37 -0300

    Added Castle reference to projects.

---
+++ Core/Components.cs
@@ -0,0 +1,47 @@
+﻿using Castle.MicroKernel;
+using Castle.Windsor;
+using Castle.Windsor.Configuration.Interpreters;
+
+namespace Core {
+    public class Components {
+        private readonly IWindsorContainer container;
+
+        public Components() : this(new WindsorContainer(new XmlInterpreter())) {}
+
+        internal Components(IWindsorContainer container) {
+            this.container = container;
+        }
+
+        private static Components instance;
+        public static Components Instance {
+            get {
+                if (instance != null) return instance;
+
+                lock (typeof (Components)) {
+                    instance = new Components();
+                }
+                return instance;
+            }
+            internal set { instance = value; }
+        }
+
+        public static IKernel Kernel {
+            get { return Instance.container.Kernel; }
+        }
+
+        public virtual T GetComponent<T>() {
+            return Instance.container.Kernel.Resolve<T>();
+        }
+
+        public virtual T GetComponent<T>(params object[] args) {
+            return Instance.container.Kernel.Resolve<T>(args);
+        }
+
+        public static void Dispose() {
+            if (instance == null) return;
+
+            instance.container.Dispose();
+            instance = null;
+        }
+    }
+}
---
+++ SqlServer/Facility.cs
@@ -0,0 +1,12 @@
+﻿using Castle.MicroKernel.Facilities;
+using Castle.MicroKernel.Registration;
+
+namespace SqlServer {
+    public class Facility : AbstractFacility {
+        protected override void Init() {
+            Kernel.Register(Types.FromThisAssembly()
+                .Pick()
+                .If(Component.IsCastleComponent));
+        }
+    }
+}
