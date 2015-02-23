a34207d3d56b2118b5471146539787ef67859547
Thiago Sá <thiagottsa@gmail.com>
2015-02-22 23:46:10 -0300

    Equals implementation for PrimaryKeyDescription

--- Core/Descriptions/PrimaryKeyDescription.cs
+++ Core/Descriptions/PrimaryKeyDescription.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Descriptions {
     public class PrimaryKeyDescription : ConstraintDescription {
@@ -6,6 +7,23 @@ namespace Core.Descriptions {
 
         public override string FullName {
             get { return string.IsNullOrWhiteSpace(Schema) ? Name : string.Format("{0}.{1}", Schema, Name); }
+        }
+
+        public override bool Equals(object other) {
+            if (!(other is PrimaryKeyDescription)) return false;
+            var indexDescription = other as PrimaryKeyDescription;
+            return base.Equals(other) &&
+                   ColumnNames.Count == indexDescription.ColumnNames.Count &&
+                   ColumnNames.All(c => indexDescription.ColumnNames.Any(f => f.Equals(c)));
+        }
+
+        public override int GetHashCode() {
+            int hashCode = base.GetHashCode();
+            foreach (var columnName in ColumnNames) {
+                hashCode ^= columnName.GetHashCode();
+            }
+
+            return hashCode;
         }
     }
 }
---
+++ Tests/Tests.Core/Descriptions/PrimaryKeyDescriptionTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using Core.Descriptions;
+using NUnit.Framework;
+
+namespace Tests.Core.Descriptions {
+    [TestFixture]
+    public class PrimaryKeyDescriptionTests {
+        [Test]
+        public void WhenPrimaryKeysHaveDifferentFullNames_EqualsMustReturnFalse() {
+            var primaryKey1 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> {"column1", "column2"}
+            };
+
+            var primaryKey2 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary2",
+                ColumnNames = new List<string> { "column1", "column2" }
+            };
+
+            var result = primaryKey1.Equals(primaryKey2);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void WhenPrimaryKeysHaveADifferentAmountOfColumnNames_EqualsMustReturnFalse() {
+            var primaryKey1 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> { "column1", "column2" }
+            };
+
+            var primaryKey2 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> { "column1" }
+            };
+
+            var result = primaryKey1.Equals(primaryKey2);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void WhenPrimaryKeysHaveDifferentColumnNames_EqualsMustReturnFalse() {
+            var primaryKey1 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> {"column1", "column2"}
+            };
+
+            var primaryKey2 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> {"column1", "column3"}
+            };
+
+            var result = primaryKey1.Equals(primaryKey2);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void WhenPrimaryKeysHaveTheSameFullNameAndColumnNames_EqualsMustReturnTrue() {
+            var primaryKey1 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> { "column1", "column2" }
+            };
+
+            var primaryKey2 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> { "column1", "column2" }
+            };
+
+            var result = primaryKey1.Equals(primaryKey2);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void WhenOneOfThePrimaryKeysIsNull_EqualsMustReturnFalse() {
+            var primaryKey1 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> { "column1", "column2" }
+            };
+
+            var result = primaryKey1.Equals(null);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void WhenOneOfTheObjectsIsNotAPrimaryKeyDescription_EqualsMustReturnFalse() {
+            var primaryKey1 = new PrimaryKeyDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "primary1",
+                ColumnNames = new List<string> { "column1", "column2" }
+            };
+
+            var unique = new UniqueDescription {
+                Schema = "dbo",
+                TableName = "Table1",
+                Name = "Default1"
+            };
+
+            var result = primaryKey1.Equals(unique);
+
+            Assert.False(result);
+        }
+    }
+}
