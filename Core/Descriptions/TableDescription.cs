34aca862029f4bb4879eb2ea6a7dad15d1ee0b55
Thiago Sá <thiagottsa@gmail.com>
2015-02-22 22:04:14 -0300

    Created Description abstract class and tests for the Equals method with column descriptions.

--- Core/Descriptions/TableDescription.cs
+++ Core/Descriptions/TableDescription.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 
 namespace Core.Descriptions {
-    public class TableDescription {
+    public class TableDescription : Description {
         public string Schema { get; set; }
         public string Name { get; set; }
         public IList<ColumnDescription> Columns { get; set; }
 
-        public string FullName {
+        public override string FullName {
             get { return string.IsNullOrWhiteSpace(Schema) ? Name : string.Format("{0}.{1}", Schema, Name); }
         }
     }
