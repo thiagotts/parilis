7ba43ea7b2c05314f9d357d0d04467769437b818
Thiago Sá <thiagottsa@gmail.com>
2015-02-22 22:04:14 -0300

    Created Description abstract class and tests for the Equals method with column descriptions.

--- Core/Actions/ConstraintAction.cs
+++ Core/Actions/ConstraintAction.cs
@@ -2,9 +2,11 @@
 
 namespace Core.Actions {
     internal abstract class ConstraintAction : Action {
+        internal readonly IDatabase Database;
         internal readonly IConstraint Constraints;
 
         protected ConstraintAction(ConnectionInfo connectionInfo) {
+            Database = Components.Instance.GetComponent<IDatabase>(connectionInfo);
             Constraints = Components.Instance.GetComponent<IConstraint>(connectionInfo);
         }
     }
---
+++ Tests/Tests.Core/Descriptions/ColumnDescriptionTests.cs
@@ -0,0 +1,54 @@
+﻿using Core.Descriptions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Tests.Core.Descriptions {
+    [TestFixture]
+    public class ColumnDescriptionTests {
+        [Test]
+        public void WhenColumnsHaveDifferentFullNames_EqualsMustReturnFalse() {
+            var column1 = Substitute.For<ColumnDescription>();
+            column1.FullName.Returns("Name 1");
+            var column2 = Substitute.For<ColumnDescription>();
+            column2.FullName.Returns("Name 2");
+
+            var result = column1.Equals(column2);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void WhenColumnsTheSameFullName_EqualsMustReturnTrue() {
+            var column1 = Substitute.For<ColumnDescription>();
+            column1.FullName.Returns("Name");
+            var column2 = Substitute.For<ColumnDescription>();
+            column2.FullName.Returns("Name");
+
+            var result = column1.Equals(column2);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void WhenOneOfTheColumnsIsNull_EqualsMustReturnFalse() {
+            var column1 = Substitute.For<ColumnDescription>();
+            column1.FullName.Returns("Name");
+
+            var result = column1.Equals(null);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void WhenOneOfTheObjectsIsNotAColumnDescription_EqualsMustReturnFalse() {
+            var column = Substitute.For<ColumnDescription>();
+            column.FullName.Returns("Name");
+            var table = Substitute.For<TableDescription>();
+            table.FullName.Returns("Name");
+
+            var result = column.Equals(table);
+
+            Assert.False(result);
+        }
+    }
+}
