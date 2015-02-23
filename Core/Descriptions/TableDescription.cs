50f73b30d6a888c66f3d5cd67ed62c3b56c01043
Thiago Sá <thiagottsa@gmail.com>
2015-02-22 23:58:41 -0300

    Equals implementation forTableDescription

--- Core/Descriptions/TableDescription.cs
+++ Core/Descriptions/TableDescription.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Descriptions {
     public class TableDescription : Description {
@@ -8,6 +9,23 @@ namespace Core.Descriptions {
 
         public override string FullName {
             get { return string.IsNullOrWhiteSpace(Schema) ? Name : string.Format("{0}.{1}", Schema, Name); }
+        }
+
+        public override bool Equals(object other) {
+            if (!(other is TableDescription)) return false;
+            var tableDescription = other as TableDescription;
+            return base.Equals(other) &&
+                   Columns.Count == tableDescription.Columns.Count &&
+                   Columns.All(c => tableDescription.Columns.Any(t => t.Equals(c)));
+        }
+
+        public override int GetHashCode() {
+            int hashCode = base.GetHashCode();
+            foreach (var column in Columns) {
+                hashCode ^= column.GetHashCode();
+            }
+
+            return hashCode;
         }
     }
 }
