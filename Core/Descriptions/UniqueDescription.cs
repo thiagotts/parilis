b63f3ea3fb2a54b762d4151aa5030759d1875776
Thiago Sá <thiagottsa@gmail.com>
2015-02-23 00:06:27 -0300

    Equals implementation for UniqueDescription

--- Core/Descriptions/UniqueDescription.cs
+++ Core/Descriptions/UniqueDescription.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Descriptions {
     public class UniqueDescription : ConstraintDescription {
@@ -6,6 +7,23 @@ namespace Core.Descriptions {
 
         public override string FullName {
             get { return string.IsNullOrWhiteSpace(Schema) ? Name : string.Format("{0}.{1}", Schema, Name); }
+        }
+
+        public override bool Equals(object other) {
+            if (!(other is UniqueDescription)) return false;
+            var uniqueDescription = other as UniqueDescription;
+            return base.Equals(other) &&
+                   ColumnNames.Count == uniqueDescription.ColumnNames.Count &&
+                   ColumnNames.All(c => uniqueDescription.ColumnNames.Any(f => f.Equals(c)));
+        }
+
+        public override int GetHashCode() {
+            var hashCode = base.GetHashCode();
+            foreach (var columnName in ColumnNames) {
+                hashCode ^= columnName.GetHashCode();
+            }
+
+            return hashCode;
         }
     }
 }
