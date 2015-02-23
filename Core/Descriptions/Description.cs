ad27e395d56f913162de33313d79e8118ae07916
Thiago Sá <thiagottsa@gmail.com>
2015-02-22 22:04:14 -0300

    Created Description abstract class and tests for the Equals method with column descriptions.

---
+++ Core/Descriptions/Description.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Core.Descriptions {
+    public abstract class Description {
+        public abstract string FullName { get; }
+
+        public override bool Equals(object other) {
+            if (ReferenceEquals(null, other)) return false;
+            if (!GetType().FullName.Equals(other.GetType().FullName)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            var description = other as Description;
+            return FullName.Equals(description.FullName, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public override int GetHashCode() {
+            return GetType().FullName.GetHashCode() ^
+                   FullName.GetHashCode();
+        }
+    }
+}
