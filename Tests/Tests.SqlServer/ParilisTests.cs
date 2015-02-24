379ed591ca420bb05e2b52ee7a613a79eb001c1a
Thiago Sá <thiagottsa@gmail.com>
2015-02-24 15:02:37 -0300

    Integration test.

---
+++ Tests/Tests.SqlServer/ParilisTests.cs
@@ -0,0 +1,30 @@
+﻿using Core;
+using Core.Descriptions;
+using NUnit.Framework;
+
+namespace Tests.SqlServer {
+    [TestFixture]
+    public class ParilisTests {
+
+        [Ignore]
+        [Test]
+        public void TestParilis() {
+            DatabaseDescription actual = new DatabaseDescription(new ConnectionInfo {
+                DatabaseName = "actual",
+                HostName = @"localhost\SQLEXPRESS",
+                User = "sa",
+                Password = "yourpassword"
+            });
+
+            DatabaseDescription reference = new DatabaseDescription(new ConnectionInfo {
+                DatabaseName = "reference",
+                HostName = @"localhost\SQLEXPRESS",
+                User = "sa",
+                Password = "yourpassword"
+            });
+
+            Parilis parilis = new Parilis(actual, reference);
+            parilis.Run();
+        }
+    }
+}
