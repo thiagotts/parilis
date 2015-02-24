d7e70500b1b2e071d588ff376ec280e182405d3f
Thiago Sá <thiagottsa@gmail.com>
2015-02-24 10:48:59 -0300

    Created the ActionQueue class.

---
+++ Core/ActionQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Castle.Core;
+using Core.Exceptions;
+using Action = Core.Actions.Action;
+
+namespace Core {
+    [CastleComponent("Core.ActionQueue", typeof (ActionQueue), Lifestyle = LifestyleType.Singleton)]
+    public class ActionQueue {
+        private readonly Queue<Action> queue;
+
+        public ActionQueue() {
+            queue = new Queue<Action>();
+        }
+
+        public int Count { get { return queue.Count; } }
+
+        internal void Push(Action action) {
+            if (action == null) throw new InvalidActionException();
+            queue.Enqueue(action);
+        }
+
+        internal Action Pop() {
+            try {
+                return queue.Dequeue();
+            }
+            catch (InvalidOperationException) {
+                return null;
+            }
+        }
+
+        internal void Clear() {
+            queue.Clear();
+        }
+    }
+}
